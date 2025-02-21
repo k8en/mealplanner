462d4c605fa7af7ad5ada761b50d8556451b53bc
k8en <enklause@yandex.ru>
2025-02-21 23:37:12 +0500

    Updated login page

--- src/main/java/org/kdepo/solutions/mealplanner/config/WebSecurityConfig.java
+++ src/main/java/org/kdepo/solutions/mealplanner/config/WebSecurityConfig.java
@@ -18,7 +18,12 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/home",
+                                "/recipes",
+                                "/styles/common.css"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
