f9a3dcf0ead3347e5d3e528dde5f269c204797a2
k8en <enklause@yandex.ru>
2025-02-24 17:28:21 +0500

    Added tags base operations

--- src/main/java/org/kdepo/solutions/mealplanner/controller/TagsController.java
+++ src/main/java/org/kdepo/solutions/mealplanner/controller/TagsController.java
@@ -1,14 +1,19 @@
 package org.kdepo.solutions.mealplanner.controller;
 
+import jakarta.validation.Valid;
 import org.kdepo.solutions.mealplanner.model.Recipe;
 import org.kdepo.solutions.mealplanner.model.Tag;
+import org.kdepo.solutions.mealplanner.repository.PrimaryKeysRepository;
 import org.kdepo.solutions.mealplanner.repository.RecipesRepository;
 import org.kdepo.solutions.mealplanner.repository.TagsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collections;
@@ -17,6 +22,11 @@ import java.util.List;
 @Controller
 @RequestMapping("/tags")
 public class TagsController {
+
+    private static final String PK = "tag_id";
+
+    @Autowired
+    private PrimaryKeysRepository primaryKeysRepository;
 
     @Autowired
     private RecipesRepository recipesRepository;
@@ -34,11 +44,11 @@ public class TagsController {
         return "tags_list";
     }
 
-    @GetMapping("/{tid}")
-    public String showDeviceDetailsPage(@PathVariable Integer tid, Model model) {
-        System.out.println("[WEB]" + " GET " + "/tags/" + tid);
+    @GetMapping("/{id}")
+    public String showTagDetailsPage(@PathVariable Integer id, Model model) {
+        System.out.println("[WEB]" + " GET " + "/tags/" + id);
 
-        Tag tag = tagsRepository.getTag(tid);
+        Tag tag = tagsRepository.getTag(id);
         if (tag != null) {
             model.addAttribute("tag", tag);
 
@@ -49,6 +59,188 @@ public class TagsController {
         } else {
             return "redirect:/tags_list";
         }
+    }
+
+    @GetMapping("/create")
+    public String showTagCreationForm(Model model) {
+        System.out.println("[WEB]" + " GET " + "/tags/create");
+
+        Tag tag = new Tag();
+        tag.setTagId(-1);
+        model.addAttribute("tag", tag);
+
+        return "tag_create";
+    }
+
+    @PostMapping("/create")
+    public String acceptTagCreationForm(@Valid Tag tag, BindingResult result) {
+        System.out.println("[WEB]" + " POST " + "/tags/create");
+
+        // Validate that this operation is allowed by the current user
+        // TODO
+
+        // Validate that provided data is correct
+        String tagName = tag.getName();
+        if (tagName == null || tagName.isEmpty()) {
+            FieldError nameFieldError = new FieldError("tag", "name", "Поле не может быть пустым!");
+            result.addError(nameFieldError);
+            return "tag_create";
+        }
+
+        if (tagName.length() > 20) {
+            FieldError nameFieldError = new FieldError("tag", "name", "Название не может быть длиннее 20 символов!");
+            result.addError(nameFieldError);
+            return "tag_create";
+        }
+
+        List<Tag> allTags = tagsRepository.getAllTags();
+        for (Tag tagToValidate : allTags) {
+            if (tagToValidate.getName().equalsIgnoreCase(tagName)) {
+                FieldError nameFieldError = new FieldError("tag", "name", "Объект с таким именем уже существует!");
+                result.addError(nameFieldError);
+                return "tag_create";
+            }
+        }
+
+        if (tag.getDescription() != null && tag.getDescription().length() > 200) {
+            FieldError nameFieldError = new FieldError("tag", "description", "Примечание не может быть длиннее 200 символов!");
+            result.addError(nameFieldError);
+            return "tag_create";
+        }
+
+        // Generate primary key for new entity
+        Integer tagId = primaryKeysRepository.getNextVal(PK);
+        primaryKeysRepository.moveNextVal(PK);
+        tag.setTagId(tagId);
+
+        // Create entity
+        tagsRepository.addTag(tag.getTagId(), tag.getName(), tag.getDescription());
+
+        // Register operation in system events log
+        // TODO
+
+        return "redirect:/tags/" + tag.getTagId();
+    }
+
+    @GetMapping("/{id}/update")
+    public String showTagModificationForm(@PathVariable Integer id, Model model) {
+        System.out.println("[WEB]" + " GET " + "/tags/" + id + "/update");
+
+        // Validate that this operation is allowed by the current user
+        // TODO
+
+        Tag tag = tagsRepository.getTag(id);
+        if (tag != null) {
+            model.addAttribute("tag", tag);
+            return "tag_update";
+        } else {
+            return "redirect:/tags";
+        }
+    }
+
+    @PostMapping("/{id}/update")
+    public String acceptTagModificationForm(@Valid Tag tag, @PathVariable Integer id, BindingResult result) {
+        System.out.println("[WEB]" + " POST " + "/tags/" + id + "/update");
+
+        // Validate that this operation is allowed by the current user
+        // TODO
+
+        // Validate that provided data is correct
+        String tagName = tag.getName();
+        if (tagName == null || tagName.isEmpty()) {
+            FieldError nameFieldError = new FieldError("tag", "name", "Поле не может быть пустым!");
+            result.addError(nameFieldError);
+            return "tag_update";
+        }
+
+        if (tagName.length() > 20) {
+            FieldError nameFieldError = new FieldError("tag", "name", "Название не может быть длиннее 20 символов!");
+            result.addError(nameFieldError);
+            return "tag_update";
+        }
+
+        List<Tag> allTags = tagsRepository.getAllTags();
+        for (Tag tagToValidate : allTags) {
+            if (tagToValidate.getName().equalsIgnoreCase(tagName)) {
+                FieldError nameFieldError = new FieldError("tag", "name", "Объект с таким именем уже существует!");
+                result.addError(nameFieldError);
+                return "tag_update";
+            }
+        }
+
+        if (tag.getDescription() != null && tag.getDescription().length() > 200) {
+            FieldError nameFieldError = new FieldError("tag", "description", "Примечание не может быть длиннее 200 символов!");
+            result.addError(nameFieldError);
+            return "tag_update";
+        }
+
+        // Validate that object is exist
+        boolean isExist = false;
+        for (Tag tagToValidate : allTags) {
+            if (tagToValidate.getTagId().equals(tag.getTagId())) {
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            return "redirect:/tags";
+        }
+
+        // Update entity
+        tagsRepository.updateTag(tag.getTagId(), tag.getName(), tag.getDescription());
+
+        // Register operation in system events log
+        // TODO
+
+        return "redirect:/tags/" + tag.getTagId();
+    }
+
+    @GetMapping("/{id}/delete")
+    public String showTagDeletionForm(@PathVariable Integer id, Model model) {
+        System.out.println("[WEB]" + " GET " + "/tags/" + id + "/delete");
+
+        // Validate that this operation is allowed by the current user
+        // TODO
+
+        Tag tag = tagsRepository.getTag(id);
+        if (tag == null) {
+            return "redirect:/tags";
+        }
+        model.addAttribute("tag", tag);
+
+        return "tag_delete";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String acceptTagDeletionForm(@PathVariable Integer id) {
+        System.out.println("[WEB]" + " POST " + "/tags/" + id + "/delete");
+
+        // Validate that this operation is allowed by the current user
+        // TODO
+
+        if (id == null) {
+            System.out.println("[WEB] Error! Tag id not provided!");
+            return "redirect:/tags";
+        }
+
+        Tag tag = tagsRepository.getTag(id);
+        if (tag == null) {
+            return "redirect:/tags";
+        }
+
+        // Delete tag bindings
+        List<Recipe> recipesByTag = recipesRepository.getAllRecipes(Collections.emptyList(), Collections.singletonList(tag.getTagId()));
+        for (Recipe recipe : recipesByTag) {
+            tagsRepository.deleteTagFromRecipe(tag.getTagId(), recipe.getRecipeId());
+        }
+
+        // Delete entity
+        tagsRepository.deleteTag(tag.getTagId());
+
+        // Register operation in system events log
+        // TODO
+
+        return "redirect:/tags";
     }
 
 }
